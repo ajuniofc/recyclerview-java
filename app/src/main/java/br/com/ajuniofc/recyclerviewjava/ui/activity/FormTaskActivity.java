d6e5b9b373bc038a9af57463420ab2447fb31f57
Antonio Junio Farias Carreiro <a.junio.fc@gmail.com>
2018-04-13 17:33:28 -0300

    Comecar Form

--- app/src/main/java/br/com/ajuniofc/recyclerviewjava/ui/activity/FormTaskActivity.java
+++ app/src/main/java/br/com/ajuniofc/recyclerviewjava/ui/activity/FormTaskActivity.java
@@ -2,11 +2,37 @@ package br.com.ajuniofc.recyclerviewjava.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import br.com.ajuniofc.recyclerviewjava.R;
 
 public class FormTaskActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_task);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.form_task_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.form_task_menu_save:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
